a5c51482dad54baf65147df9319d1991659254ea
bexultanrakhim <bexultan.rakhim@nu.edu.kz>
2018-07-04 10:53:45 +0600

    Update chain_rule.m

--- chain_rule.m
+++ chain_rule.m
@@ -1,4 +1,5 @@
 function p_dot = chain_rule(p,theta,theta_dot)
+%this function uses vector of cartesian coordinates as function of "theta" and by chain rule finds proper differentiation on time
     p_dot = sym(zeros(size(p)));
     for ind = 1:length(theta) 
         p_dot = p_dot + diff(p,theta(ind))*theta_dot(ind);
